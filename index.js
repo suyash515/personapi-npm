70e710caa99965000f7f9cd13f3d744ad580a274
Suyash Sumaroo <suyash515@gmail.com>
2018-02-07 15:30:18 +0400

    added additional function

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 'use strict';
 
-/**
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
-module.exports = function(token) {
-	console.log(token);//debug
+var request = require("request");
+
+module.exports = {
+    personapiUrl: "https://api.personapi.com",
+    token: "",
+    actions: {
+        registerUser: "/appuser/create"
+    },
+
+    init: function(token) {
+        this.token = token;
+    },
+
+    setServiceURL: function(url) {
+        if (url.trim() != "") {
+            this.personapiUrl = url;
+        }
+    },
+
+    registerUser: function(params) {
+        params.token = this.token;
+
+        return this._sendRequest(this.actions.registerUser, params);
+    },
+
+    _sendRequest: function(action, params) {
+        var url = this.personapiUrl;
+
+        return new Promise(function(fulfill, reject) {
+            var options = {
+                uri: url + action,
+                method: "POST",
+                json: params
+            };
+
+            request(options, function(error, response, body) {
+                if(error) {
+                    reject(error);
+                } else {
+                    fulfill(body);
+                }
+            });
+        });
+    }
 };
