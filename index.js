c4673e668e0dfe9f9f6b7c3ca787c5e05b6aea94
Suyash Sumaroo <suyash515@gmail.com>
2018-07-05 14:22:44 +0400

    updated docs and updated url to link to personapi.codevigor.com

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var request = require("request");
 
 module.exports = {
-    personapiUrl: "https://api.personapi.com",
+    personapiUrl: "https://personapi.codevigor.com",
     userToken: "",
     appToken: "",
     actions: {
